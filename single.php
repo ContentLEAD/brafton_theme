3fcb027257f064f5c66662faff3f810d2081a680
Mike <mdewitt07@gmail.com>
2014-12-19 12:37:33 -0700

    fix tagbar and body formatting issues in single.php

--- single.php
+++ single.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 	<div id="content">
-		<div class="d-all tagbar">
-			<?php blog_tagbar(); //see functions.php ?>
-		</div>
 		<article itemscope itemtype="http://schema.org/<?php if(in_category('blog')) echo 'Blog'; else echo 'Article'; ?>" id="single" class="d-all row">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 				$author = brafton_author_data( get_the_ID() ); ?>
 
 				<div class="d-all blog_header" style="overflow: visible">
+					<div class="d-all tagbar">
+						<?php blog_tagbar(); //see functions.php ?>
+					</div>
 					<?php //If this is a video post, change things around a bit
 					//Or, if it's a twit, echo the twit custom field
 					//SEE- just above the_content();
@@ -51,7 +51,6 @@
 				</div>
 				<?php if( function_exists( 'brafton_share' ) ) brafton_share( 'top' ); ?>
 				<div class="d-all row">
-				<div id="body" class="d-2of3" itemprop="text">
 
 					<?php if( $video_shortcode ) { 
 						echo do_shortcode( $video_shortcode );
@@ -62,54 +61,53 @@
 					<?php } ?>
 
 					<div id="inner-content" class="wrap cf">
-						<section class="entry-content cf" itemprop="articleBody">
-							<?php the_content(); ?>
-						</section>
-					</div>
-					<?php if( function_exists( 'brafton_share' )) brafton_share( 'bottom' ); ?>
-					<footer class="meta">
-						<?php if(get_the_author() != 'Editorial') { ?>
-						<div class="boilerplate">
-							<table><td class="author_avatar_container" style="background:#000"><a href="<?php echo $author['url']; ?>" class="user"><?php echo get_avatar($author['ID']); ?></a></td><td class="bio"><?php echo $author['description']; ?></td></table>
-							<a href="<?php echo $author['url']; ?>" title="View <?php echo $author['name']; ?>'s Profile" class="authorprofile"><img src="/wp-content/themes/b3/blog-images/auth_profile.png"/></a>
-							<div class="icons small">
-							<?php if( $author['linkedin'] ) echo "<a href='".$author['linkedin']."' class='cube linkedin'></a>"; ?> 
-							<?php if( $author['twitter'] ) echo "<a href='http://www.twitter.com/".$author['twitter']."' class='cube twitter'></a>"; ?> 
-							<?php if( $author['facebook'] ) echo "<a href='http://www.facebook.com/".$author['facebook']."' class='cube facebook'></a>"; ?> 
-							<?php if( $author['googleplus_id'] ) echo "<a href='https://plus.google.com/".$author['googleplus_id']."?rel=author' class='cube gplus'></a>"; ?></div>
+						<section class="entry-content d-2of3 t-2of3 m-all cf" itemprop="articleBody">
+								<?php the_content(); ?>
+									<?php if( function_exists( 'brafton_share' )) brafton_share( 'bottom' ); ?>
+									<footer class="meta">
+										<?php if(get_the_author() != 'Editorial') { ?>
+										<div class="boilerplate">
+											<table><td class="author_avatar_container" style="background:#000"><a href="<?php echo $author['url']; ?>" class="user"><?php echo get_avatar($author['ID']); ?></a></td><td class="bio"><?php echo $author['description']; ?></td></table>
+											<a href="<?php echo $author['url']; ?>" title="View <?php echo $author['name']; ?>'s Profile" class="authorprofile"><img src="/wp-content/themes/b3/blog-images/auth_profile.png"/></a>
+											<div class="icons small">
+											<?php if( $author['linkedin'] ) echo "<a href='".$author['linkedin']."' class='cube linkedin'></a>"; ?> 
+											<?php if( $author['twitter'] ) echo "<a href='http://www.twitter.com/".$author['twitter']."' class='cube twitter'></a>"; ?> 
+											<?php if( $author['facebook'] ) echo "<a href='http://www.facebook.com/".$author['facebook']."' class='cube facebook'></a>"; ?> 
+											<?php if( $author['googleplus_id'] ) echo "<a href='https://plus.google.com/".$author['googleplus_id']."?rel=author' class='cube gplus'></a>"; ?></div>
+										</div>
+										<hr />
+									<?php } ?>
+									</footer>
+									<?php do_action('after_post_content'); ?>
+									<div id="comments">
+										<div class="arrow_to_infinity"></div>
+										<h1>What say you?</h1>
+										<?php comments_template(); ?>
+									</div>
+							</section>
+						<div class="sidebar d-1of3 last">
+							<?php if ( !function_exists( "dynamic_sidebar" ) || !dynamic_sidebar( "Single Sidebar" ) ) : ?>
+								<p>Add Widgets to the Post Sidebar</p>
+							<?php endif; ?>
 						</div>
-						<hr />
-					<?php } ?>
-					</footer>
-					<?php do_action('after_post_content'); ?>
-					<div id="comments">
-						<div class="arrow_to_infinity"></div>
-						<h1>What say you?</h1>
-						<?php comments_template(); ?>
 					</div>
-				</div>
-				<div class="sidebar d-1of3 last">
-					<?php if ( !function_exists( "dynamic_sidebar" ) || !dynamic_sidebar( "Single Sidebar" ) ) : ?>
-						<p>Add Widgets to the Post Sidebar</p>
-					<?php endif; ?>
-				</div>
-			</div>
+
 				<?php endwhile; endif; ?>
 		</article><!-- End #single -->
 	</div>
 
-		<div class="bottom-cta d-all">
-			<div class="bottom-cta-container">
-				<a href="http://www.brafton.com/resources/reduce-reuse-recycle-repurpose-get-content"><div class="ourlatestfooter">
-				</div></a>
+	<div class="bottom-cta d-all">
+		<div class="bottom-cta-container">
+			<a href="http://www.brafton.com/resources/reduce-reuse-recycle-repurpose-get-content"><div class="ourlatestfooter">
+			</div></a>
 
-				<a href="http://www.brafton.com/news/connect-brafton-leadscon-new-york-aug-14-15-leadscon"><div class="leadscon">
-				</div></a>
+			<a href="http://www.brafton.com/news/connect-brafton-leadscon-new-york-aug-14-15-leadscon"><div class="leadscon">
+			</div></a>
 
-				<div class="askamarketer">
-				</div>
+			<div class="askamarketer">
 			</div>
-		</div>	
+		</div>
+	</div>	
 
 
 
