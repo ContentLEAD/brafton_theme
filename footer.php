915bf5d04483e6d10a0161b302604585c06e3420
Mike <mdewitt07@gmail.com>
2014-12-26 12:39:45 -0700

    finish top-footer

--- footer.php
+++ footer.php
@@ -19,7 +19,26 @@
 
 							?>
 						</nav>
+
+						<div class="footer-cta-container d-all t-all">
+							<div class="footer-social-icons">
+								<a rel="nofollow" href="http://www.linkedin.com/company/290802" class="cube footer-linkedin" title="Connect with Brafton on LinkedIn"></a>
+								<a rel="nofollow" href="https://www.twitter.com/Brafton" class="cube footer-twitter" title="Follow Brafton on Twitter"></a>
+								<a rel="nofollow" href="https://www.facebook.com/Brafton" class="cube footer-facebook" title="Like Brafton on Facebook"></a>
+								<a rel="nofollow" href="https://plus.google.com/114039629718111030960" class="cube footer-gplus" title="Add Brafton to your circles on Google+"></a>
+								<a rel="nofollow" href="http://www.pinterest.com/brafton" class="cube footer-pinterest" title="Check out Brafton's boards on Pinterest"></a>
+								<a rel="nofollow" href="http://www.youtube.com/user/BraftonCustomNews" class="cube footer-youtube" title="Subscribe to Brafton on YouTube"></a>
+								<a rel="nofollow" href="http://old.brafton.com/feed " class="cube footer-rss" title="Subscribe to Brafton's RSS feed"></a>
+								<div class="follow-us">FOLLOW US</div>
+							</div>
+							<div class="footer-contact-us">
+								<a href="/contact">CONTACT US</a>
+							</div>
+						</div>
+
+
 					</div>
+
 				<?php } ?>
 
 				<div id="inner-footer" class="wrap cf">
